7421b2a5b2a72ee6f82ea2a8f12b2b2a8db24ac6
Albert Canales Ros <albertcanalesros@gmail.com>
2021-09-05 09:38:05 +0200

    Improvements on sxhkd and bspwm configs

--- bspwm/rules.sh
+++
@@ -1,9 +0,0 @@
-# Rules for bspwm
-
-bspc rule -a Zathura state=tiled
-bspc rule -a Joplin desktop=10
-bspc rule -a Todoist desktop=4
-bspc rule -a whatsapp-nativefier-d52542 desktop=9
-bspc rule -a TelegramDesktop desktop=9
-
-
